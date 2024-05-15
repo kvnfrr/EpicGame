b2f40d14d1906feaa5f5ce4eee988c356b35a38f
TropicalChop <jonathanfprod@gmail.com>
2024-05-15 09:09:28 -0400

    Player Instance and minor desktop updates
    added player movement method
    added instance of player and texture
    updated background
    updated window size

--- core/src/com/epic/game/Game.java
+++ core/src/com/epic/game/Game.java
@@ -1,31 +1,60 @@
 package com.epic.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private Rectangle player;
+	private Texture playerImage;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
 	@Override
 	public void create () {
+
+		playerImage = new Texture("player-temp.png");
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		player = new Rectangle();
+		player.x = 100;
+		player.y = 100;
+		player.width = 30;
+		player.height = 50;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(playerImage, player.x, player.y);
 		batch.end();
+
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		playerImage.dispose();
 	}
+
+	private void playerMovement(){
+
+		//tbd aka idk yet
+
+	}
+
 }
--- desktop/src/com/epic/game/DesktopLauncher.java
+++ desktop/src/com/epic/game/DesktopLauncher.java
@@ -9,7 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Epic Game");
+		config.setTitle("Turn-Based Game");
+		config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new Game(), config);
 	}
 }
