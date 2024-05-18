a9e33cd467b10bf28dcb2220d93befcefc62cdf2
TropicalChop <jonathanfprod@gmail.com>
2024-05-18 17:34:21 -0400

    Updated Movement
    Updated A and D keys to turn about player origin
    Updated W key to act like thruster forward
    Cleaned up code

--- core/src/com/epic/game/Game.java
+++ core/src/com/epic/game/Game.java
@@ -20,22 +20,20 @@ public class Game extends ApplicationAdapter {
 	private float acceleration = 200f;
 	private float maxSpeed = 400f;
 	private float deceleration = 200f;
-	private float rotationSpeed = 100f;
 	//simple movement variables, must be float because of batch
 	private float xAxis = 600f;
 	private float yAxis = 350f;
+    private float rotationAngle = 90f; // starting angle (90 degrees is north)
 
 	@Override
 	public void create () {
 
-		//load images
+		//load images (SPRITE MUST BE FACING RIGHT) sprite and rotaion not linked otherwise
 		player = new Texture("player.png");
 
 		//instance of sprite batch (used to make character a rectangle p much)
 		batch = new SpriteBatch();
 
-		//player movement
-		userInput();
 	}
 
 	@Override
@@ -43,32 +41,44 @@ public class Game extends ApplicationAdapter {
 		ScreenUtils.clear(0,0,0,1);	//sets background back to black after input, prevents after image
 
 		batch.begin();
-		batch.draw(player, xAxis, yAxis);
-		batch.end();
+		// Reminder: creates player (player(image), xAxis, yAxis (for starting position), xOrigin, yOrigin (for centering rotation), xScale, yScale (for size), rotationAngle (value for direction to face), srcX, srY (no idea),
+		batch.draw(player, xAxis - player.getWidth() / 2, yAxis - player.getHeight() / 2, player.getWidth() / 2, player.getHeight() / 2, player.getWidth(), player.getHeight(), 1, 1, rotationAngle, 0, 0, player.getWidth(), player.getHeight(), false, false);		batch.end();
 
 		userInput();	//handles player movement
+
 	}
+
+
 
 	@Override
 	public void dispose() {
-		super.dispose();
+		player.dispose();
+		batch.dispose();
 	}
 
 	private void userInput() {
-		if(Gdx.input.isKeyPressed(Input.Keys.W)){
-			yAxis += 7;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.A)){
-			xAxis -= 7;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.S)){
-			yAxis -= 7;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D)){
-			xAxis += 7;
-		}
+		// Trig used for getting W to move forward based on position looking
+		float radianAngle = (float) Math.toRadians(rotationAngle);
 
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			xAxis += Math.cos(radianAngle) * 7;
+			yAxis += Math.sin(radianAngle) * 7;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			rotationAngle += 5;
+			if (rotationAngle >= 360) {
+				rotationAngle -= 360;
+			}
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+			// Plan to add momentum and this would be like a brake button, if not just use W key code and convert + to - for reverse.
+
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			rotationAngle -= 5;
+			if (rotationAngle < 0) {
+				rotationAngle += 360;
+			}
+		}
 	}
-
-
 }
